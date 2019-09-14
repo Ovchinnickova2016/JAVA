e7b228b60ed0f0f8f196685684ece7c27e9da3f7
Anastasiya <ovchinnickova.anast@gmail.com>
2019-09-14 14:15:49 +0500

    Corrected 2nd task

--- Sanbox/src/main/java/ru/stq/java/sandbox/MyFirstProgram.java
+++ Sanbox/src/main/java/ru/stq/java/sandbox/MyFirstProgram.java
@@ -3,8 +3,8 @@ package ru.stq.java.sandbox;
 public class MyFirstProgram{
 	public static void main (String [] args){
     System.out.println("Hello world!");
-    Point points = new Point(1.1, 2.1, 4.1, 3.1);
-    double dist = points.distance();
-    System.out.println("Расстояние между двумя точками: "+ points.distance());
+    Point p1 = new Point(1, 2);
+    Point p2 = new Point(9, 4);
+    System.out.println("Расстояние между двумя точками: "+ p1.distance(p2));
 	}
 }
--- Sanbox/src/main/java/ru/stq/java/sandbox/Point.java
+++ Sanbox/src/main/java/ru/stq/java/sandbox/Point.java
@@ -1,27 +1,19 @@
 package ru.stq.java.sandbox;
 
 public class Point {
-  double x1, y1, x2, y2;
-  public Point(double x1, double y1, double x2, double y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  double x, y;
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
   }
-  public double getX1() {
-    return x1;
+  public double getX() {
+    return x;
   }
-  public double getY1() {
-    return y1;
-  }
-  public double getX2() {
-    return x2;
-  }
-  public double getY2() {
-    return y2;
+  public double getY() {
+    return y;
   }
 
-  public double distance() {
-    return Math.sqrt(Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2));
+  public double distance(Point p) {
+    return Math.sqrt(Math.pow((p.x - this.x), 2) + Math.pow((p.y - this.y), 2));
   }
 }
