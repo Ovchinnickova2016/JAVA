10697f3b67912d31a36754e8dd659ea836cf1b84
Anastasiya <ovchinnickova.anast@gmail.com>
2019-09-14 14:21:02 +0500

    Corrected 3rd task

--- Sanbox/src/test/java/ru/stqa/java/sandbox/PointTest.java
+++ Sanbox/src/test/java/ru/stqa/java/sandbox/PointTest.java
@@ -7,12 +7,15 @@ public class PointTest {
 
   @Test
   public void testArea(){
-    Point points = new Point(1, 2, 4, 2);
-    Assert.assertEquals(points.distance(), 3.0);
-    Point points2 = new Point(1, 5, 1, 0);
-    Assert.assertEquals(points2.distance(), 5.0);
-    Point points3 = new Point(2, 5, 3, 5);
-    Assert.assertEquals(points3.distance(), 1.0);
+    Point p1 = new Point(1.0, 2.0);
+    Point p2 = new Point(4.0, 2.0);
+    Assert.assertEquals(p1.distance(p2), 3.0);
+    Point p3 = new Point(1.0, 5.0);
+    Point p4 = new Point(1.0, 0.0);
+    Assert.assertEquals(p3.distance(p4), 5.0);
+    Point p5 = new Point(2.0, 5.0);
+    Point p6 = new Point(3.0, 5.0);
+    Assert.assertEquals(p5.distance(p6), 1.0);
   }
 
 }
