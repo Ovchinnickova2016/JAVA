611cf8c6c1cadc4eb4e822a12db76ee6b61dcf9b
Anastasiya <ovchinnickova.anast@gmail.com>
2019-09-23 10:42:45 +0500

    Optimized GroupCreationTest null filling forms

--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -9,7 +9,7 @@ import ru.stq.java.addressbook.model.ContactsData;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
