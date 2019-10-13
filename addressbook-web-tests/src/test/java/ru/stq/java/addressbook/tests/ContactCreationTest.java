36eceff8e973392db698ab5f1c0fac693b35bbf7
Anastasiya <ovchinnickova.anast@gmail.com>
2019-10-13 13:31:48 +0500

    2 tests of creation are parameterized

--- addressbook-web-tests/src/test/java/ru/stq/java/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stq/java/addressbook/tests/ContactCreationTest.java
@@ -3,21 +3,32 @@ package ru.stq.java.addressbook.tests;
 import org.testng.annotations.*;
 import ru.stq.java.addressbook.model.Contacts;
 import ru.stq.java.addressbook.model.ContactsData;
+import ru.stq.java.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    File photo = new File("src/test/resources/icon.png");
+    list.add(new Object[]{new ContactsData().withPhoto(photo).withEmail("ovchinnickova.anast@gmail.com").withEmail2("ovch@gmail.com").withEmail3("galim@gmail.com").withFirstName("nastya").withLastName("Ovchinnickova").withMobilePhone("44423422").withWorkPhone("231").withHomePhone("3232").withAddress("2 dd 3").withGroup("group3")});
+    list.add(new Object[]{new ContactsData().withPhoto(photo).withEmail("ovchinnickova.anast@gmail.com").withEmail2("ovch@gmail.com").withEmail3("galim@gmail.com").withFirstName("nastya2").withLastName("Ovchinnickova2").withMobilePhone("44423422").withWorkPhone("231").withHomePhone("3232").withAddress("2 dd 3").withGroup("group3")});
+    list.add(new Object[]{new ContactsData().withPhoto(photo).withEmail("ovchinnickova.anast@gmail.com").withEmail2("ovch@gmail.com").withEmail3("galim@gmail.com").withFirstName("nastya3").withLastName("Ovchinnickova3").withMobilePhone("44423422").withWorkPhone("231").withHomePhone("3232").withAddress("2 dd 3").withGroup("group3")});
+    return list.iterator();
+  }
 
+  @Test(dataProvider = "validGroups")
+  public void testContactCreation(ContactsData contact) throws Exception {
     Contacts before = app.contact().all();
     app.contact().goToContactsPage();
-    File photo = new File("src/test/resources/icon.png");
-    ContactsData contact = new ContactsData().withPhoto(photo).withEmail("ovchinnickova.anast@gmail.com").withEmail2("ovch@gmail.com").withEmail3("galim@gmail.com").withFirstName("nastya").withLastName("Ovchinnickova").withMobilePhone("44423422").withWorkPhone("231").withHomePhone("3232").withAddress("2 dd 3").withGroup("group3");
     app.contact().create(contact);
     assertThat(app.contact().count(),equalTo(before.size() +1));
     Contacts after = app.contact().all();
