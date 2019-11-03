3ac52238859c99605669c9427ba34ed986a5e847
Anastasiya <ovchinnickova.anast@gmail.com>
2019-11-03 19:24:07 +0500

    8.7

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -20,10 +20,11 @@ public class RegistrationHelper extends HelperBase {
 
   }
 
-  public void finish(String confirmationLink, String password) {
+  public void finish(String confirmationLink, String username, String password) {
     wd.get(confirmationLink);
+    type(By.name("realname"), username);
     type(By.name("password"),password);
-    type(By.name("password_confirm"),password);
-    click(By.cssSelector("input[value='Update User']"));
+    type(By.name("password"),password);
+    click(By.cssSelector("button[type='submit']"));
   }
 }
